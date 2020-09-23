5a359528aaf79e1e89cc5fa7d154765d74e64963
Olivia Lan <568854812@qq.com>
2020-09-23 11:16:27 +0800

    update

--- affective_infographics/js/firebase_config.js
+++ affective_infographics/js/firebase_config.js
@@ -2,27 +2,6 @@
 //同时还需要开通database和storage
 //database里rule里，Change allow read, write: if false; to true;
 
-// var firebaseConfig = {
-//     apiKey: "",
-//     authDomain: "",
-//     databaseURL: "",
-//     projectId: "",
-//     storageBucket: "",
-//     messagingSenderId: "",
-//     appId: ""
-// };
-
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCRNey3IkLSxaaI6Ra0Dun70k5OWuKNbTA",
-//     authDomain: "flowflow-bdc7d.firebaseapp.com",
-//     databaseURL: "https://flowflow-bdc7d.firebaseio.com",
-//     projectId: "flowflow-bdc7d",
-//     storageBucket: "flowflow-bdc7d.appspot.com",
-//     messagingSenderId: "482398842104",
-//     appId: "1:482398842104:web:c5ef36a07a97bff6cfdf0d",
-//     measurementId: "G-TG7W1494PT"
-// };
-
 var firebaseConfig = {
     apiKey: "AIzaSyAx1hju2rRP2Bsy04Y-DGZM55nDrPAo3Cs",
     authDomain: "affective-11166.firebaseapp.com",
