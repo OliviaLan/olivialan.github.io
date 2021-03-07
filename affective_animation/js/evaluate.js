ad8e03a7ae4767a10d102614e3fc5578f4e01bb4
Olivia Lan <568854812@qq.com>
2021-03-07 16:52:24 +0800

    update

--- affective_animation/js/evaluate.js
+++ affective_animation/js/evaluate.js
@@ -133,7 +133,7 @@ var evaluate = () => {
         d.append(textarea_3)
     }
 
-    d.innerHTML += '<br><br><p><strong>- What emotional message do you think the storyteller wants to communicate?</strong></p><br>'
+    d.innerHTML += '<br><br><p><strong>- What emotional message/feeling do you think the storyteller wants to communicate?</strong></p><br>'
     textarea_4 = document.createElement('textarea')
     textarea_4.id = "message"
     textarea_4.style.width = "500px"
