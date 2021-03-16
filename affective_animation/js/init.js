379075e0fe937ac6ee49c49978f9fcdc626c0b37
Olivia Lan <568854812@qq.com>
2021-03-16 19:07:05 +0800

    update

--- affective_animation/js/init.js
+++ affective_animation/js/init.js
@@ -269,7 +269,7 @@ async function find_mid() {
 // }
 
 function generateStory() {
-    var random = [1, 7, 8, 10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 27, 29];
+    var random = [7, 8, 13, 14, 15, 17, 18, 22, 23, 24, 29];
     shuffle(random)
     return (random)
 }
